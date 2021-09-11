ea5b40c522788a03df049ee2d55e2eb78c60753f
Arthur Baker <artbaker82@gmail.com>
2021-09-11 09:32:12 -0700

    rendering data to study screen

--- Components/SetButton.js
+++ Components/SetButton.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-const SetButton = ({key, name}) => {
-    return (
-        <View >
-            <Text id={key}>{name}</Text>
-        </View>
-    )
-}
+const SetButton = ({ key, name }) => {
+  return (
+    <View>
+      <Text id={key}>{name}</Text>
+    </View>
+  );
+};
+
+export default SetButton;
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -3,19 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 import Set from "../Components/Set";
 import SetButton from "../Components/SetButton";
 
-import DATA from "../TEST_DATA/DATA";
-
+import { DATA } from "../TEST_DATA/DATA";
+// console.log(DATA.userData);
 const HomeScreen = () => {
   //const {allSets} = this.props //from redux store
   const [data, setData] = useState(DATA);
-  console.log(typeof data);
+  console.log(data.userData);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Study Screen</Text>
-
-      {data.map((item, idx) => (
-        <SetButton key={idx} name={item} />
-      ))}
+      {data.userData.map((set) => {
+        return <SetButton key={set.id} name={set.setName} />;
+      })}
     </View>
   );
 };
