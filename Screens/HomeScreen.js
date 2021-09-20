c5c2088ba6a4656d3b428869dc0d12d36ffe4ce0
dales <dalesimmons0210@gmail.com>
2021-09-20 14:33:33 -0700

    homescreen

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,24 +1,33 @@
+import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
 import { connect } from "react-redux";
-
 import SetButton from "../Components/SetButton";
 
-import { DATA } from "../TEST_DATA/DATA";
-// console.log(DATA.userData);
 const HomeScreen = ({ userData }) => {
-  //const {allSets} = this.props //from redux store
+  
+  const navigate = useNavigation()
+  
+  const renderItem = ({ item }) => <SetButton setName={item.setName} id={item.id} onPress={() => navigate.navigation('ReviewScreen')}/>;
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Study Screen</Text>
-      {userData.map((set) => {
-        return <SetButton key={set.id} name={set.setName} />;
-      })}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>My Sets</Text>
+      <FlatList data={userData} renderItem={renderItem} keyExtractor={(item) => item.id} />
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F5F8FF",
+    height: "100%",
+  },
+  header: {
+    fontSize: 30,
+    alignSelf: "center",
+    marginVertical: 15,
+  },
+});
 
 const mapStateToProps = (state) => ({
   userData: state.userData.userData,
